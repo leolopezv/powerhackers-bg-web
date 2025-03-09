71ee1f458a961819e1f667d20219172cf85cb920
ssierraintibytes <ssierra@intibytes.com>
2025-03-09 12:16:46 -0500

    Version preliminar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreditPage from "@/credito/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<CreditPage />} />
+    </Routes>
+  </BrowserRouter>
+);
